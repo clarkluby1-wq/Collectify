ccaf47da596c1344a397c189a8b0b621a2eac53a
clarkluby1-wq <clarkluby1@gmail.com>
2026-09-12 19:54:14 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'collectify-v3';
+const CACHE = 'collectify-v4';
 const SHELL = ['./', 'index.html', 'style.css', 'app.js', 'api.js', 'config.js', 'manifest.json', 'icon.svg'];
 
 self.addEventListener('install', e => {
